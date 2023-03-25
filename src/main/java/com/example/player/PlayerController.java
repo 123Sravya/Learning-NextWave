643c8f125574fbfd0eba5bc1af95174c0f303172
Turpu Sravya <sravyareddy.turpu@gmail.com>
2023-03-25 07:36:33 +0000

    add crud operations like add,delete,update,edit for player

---
+++ src/main/java/com/example/player/PlayerController.java
@@ -0,0 +1,49 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.ArrayList;
+ * 
+ */
+
+// Write your code here.
+
+
+
+package com.example.player; 
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+ 
+
+// Write your code here.
+
+@RestController
+public class PlayerController{
+    PlayerService ps = new PlayerService();
+
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayers(){
+        return ps.getPlayers();
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player){
+        return ps.addPlayer(player);
+    }
+
+    @GetMapping("/players/{playerId}")
+    public Player getPlayer(@PathVariable("playerId") int playerId){
+      return ps.getPlayer(playerId);
+    }
+
+    @PutMapping("/players/{playerId}")
+    public Player updatePlayer(@PathVariable("playerId") int playerId, @RequestBody Player player){
+        return ps.updatePlayer(playerId,player);
+    }
+
+    @DeleteMapping("/players/{playerId}")
+    public void deletePlayer(@PathVariable("playerId") int playerId){
+        ps.deletePlayer(playerId);
+        }
+    
+}
